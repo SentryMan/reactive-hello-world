096b7643e0f86f06bfa5d0248f7447548554516a
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2020-08-25 00:54:46 -0400

    wrapped blocking calls

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -11,16 +11,29 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-@RestController
+@Component
 public class Controller {
 
   private static final String ENDPOINT = "http://<your-local-ip>:9090";
 
   private final RestTemplate template = new RestTemplate();
 
-  @GetMapping("helloworld")
-  String handle() {
-    return template.getForObject(ENDPOINT + "/hello", String.class)
-        + template.getForObject(ENDPOINT + "/world", String.class);
+  @Bean
+  public RouterFunction<ServerResponse> router() {
+
+    return RouterFunctions.route(GET("helloworld"), this::handle);
+  }
+
+ Mono<ServerResponse> handle(ServerRequest request) {
+
+    Mono<String> helloMono =
+        Mono.fromCallable(() -> template.getForObject(ENDPOINT + "/hello", String.class))
+            .subscribeOn(Schedulers.boundedElastic());
+
+    Mono<String> worldMono =
+        Mono.fromCallable(() -> template.getForObject(ENDPOINT + "/world", String.class))
+            .subscribeOn(Schedulers.boundedElastic());
+
+    return helloMono.zipWith(worldMono, (h, w) -> h + w).flatMap(ServerResponse.ok()::bodyValue);
   }
 }
